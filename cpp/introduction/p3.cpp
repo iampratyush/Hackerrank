f096c2d3bfc164637e35ff04dc04189102d2278c
Pratyush singh <pratyushsingh93@gmail.com>
2020-04-09 17:24:25 +0530

    problem url
    https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem

---
+++ cpp/introduction/p3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int d;
+    long int ld;
+    char c;
+    float f;
+    double lf;
+    //take input
+    scanf("%d %ld %c %f %lf",&d,&ld,&c,&f,&lf);
+
+    //output
+    printf("%d\n%ld\n%c\n%f\n%lf\n",d,ld,c,f,lf);
+   
+    return 0;
+}
